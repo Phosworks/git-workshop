8f5c196fd977a888a9d7892c75c9147069785941
marbet-zz <martin.e.betz@gmail.com>
2017-01-20 14:19:42 +0100

    Ny bugg!

--- src/entry.js
+++ src/entry.js
@@ -3,7 +3,7 @@ var $velocity = require("velocity-animate");
 
 $(function () {
     $("h1")
-    .addClass("yellow")
+    .addClass("yellowBUG")
     .text("Rock'n roll all night, party every day!")
     .velocity({ left: 800 }, { duration: 4500, easing: "swing"})
     .velocity("reverse", { loop: true });
