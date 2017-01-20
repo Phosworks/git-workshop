c793dcfcadee821f582f1ee4e68d538c3db000ca
marbet-zz <martin.e.betz@gmail.com>
2017-01-20 13:51:29 +0100

    Rock'n roll

--- src/entry.js
+++ src/entry.js
@@ -4,7 +4,7 @@ var $velocity = require("velocity-animate");
 $(function () {
     $("h1")
     .addClass("red")
-    .text("The H1 is red")
+    .text("The H1 is rock'n roll")
     .velocity({ left: 800 }, { duration: 4500, easing: "swing"})
     .velocity("reverse", { loop: true });
 })
