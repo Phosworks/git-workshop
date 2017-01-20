c96b46609b351cffa50774fc0831698a433455c4
Wincent <david.wincent@gmail.com>
2017-01-20 13:52:21 +0100

    blue

--- src/entry.js
+++ src/entry.js
@@ -3,7 +3,7 @@ var $velocity = require("velocity-animate");
 
 $(function () {
     $("h1")
-    .addClass("red")
+    .addClass("blue")
     .text("The H1 is rock'n roll")
     .velocity({ left: 800 }, { duration: 4500, easing: "swing"})
     .velocity("reverse", { loop: true });
