cd81b4459d95d6a8ced5c90a79ca5339e9f9eaed
Wincent <david.wincent@gmail.com>
2017-01-20 13:49:48 +0100

    red

--- src/entry.js
+++ src/entry.js
@@ -3,8 +3,8 @@ var $velocity = require("velocity-animate");
 
 $(function () {
     $("h1")
-    .addClass("green")
-    .text("The H1 is green")
+    .addClass("red")
+    .text("The H1 is red")
     .velocity({ left: 800 }, { duration: 4500, easing: "swing"})
     .velocity("reverse", { loop: true });
 })
